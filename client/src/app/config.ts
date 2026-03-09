0bc3bc57fab299bf78ad9b16cc5015ae45ee3a50
dxenos009@gmail.com <dxenos009@gmail.com>
2026-03-09 20:27:51 +0200

    added resolve base url in config.ts

--- client/src/app/config.ts
+++ client/src/app/config.ts
@@ -1,5 +1,12 @@
+const resolveBaseUrl = (): string => {
+  if (window.location.hostname === "localhost") {
+    return "http://localhost:5256/api/";
+  }
+  return "https://spirited-tranquility-production.up.railway.app/api/";
+};
+
 const config = {
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5256/api/",
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || resolveBaseUrl(),
 };
 
 export default config;
